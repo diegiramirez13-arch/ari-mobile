e0bef545fb5b13425f1565a826105992c82376f3
diegiramirez13-arch <diegi.ramirez13@gmail.com>
2026-01-25 03:56:48 -0300

    Update projects_repository.dart

--- app/lib/features/projects/projects_repository.dart
+++ app/lib/features/projects/projects_repository.dart
@@ -1,13 +1,27 @@
+
 import 'project.dart';
+import 'projects_storage.dart';
 
 class ProjectsRepository {
+  final ProjectsStorage storage;
   final List<Project> _projects = [];
+
+  ProjectsRepository({ProjectsStorage? storage})
+      : storage = storage ?? ProjectsStorage();
 
   List<Project> getAll() {
     return List<Project>.from(_projects);
   }
 
-  void add(Project project) {
+  Future<void> load() async {
+    final loaded = await storage.loadProjects();
+    _projects
+      ..clear()
+      ..addAll(loaded);
+  }
+
+  Future<void> add(Project project) async {
     _projects.add(project);
+    await storage.saveProjects(_projects);
   }
 }
